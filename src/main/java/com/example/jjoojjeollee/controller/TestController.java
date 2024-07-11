cb0b1480ce794f829e209983919496afd6c1757c
eungyeol0506 <janess1@naver.com>
2024-07-11 22:47:08 +0900

    24.07.11 DB connect test

--- src/main/java/com/example/jjoojjeollee/controller/TestController.java
+++ src/main/java/com/example/jjoojjeollee/controller/TestController.java
@@ -1,15 +1,26 @@
 package com.example.jjoojjeollee.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+//    @GetMapping("test")
+//    public String test(Model model){
+//        model.addAttribute("data", "hello!");
+//        return "test";
+//    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("test")
-    public String test(Model model){
-        model.addAttribute("data", "hello!");
-        return "test";
+    public String testConnection() {
+        String sql = "SELECT 'Success' FROM dual";
+        return jdbcTemplate.queryForObject(sql, String.class);
     }
 }
